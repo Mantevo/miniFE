9bc93d2bb5deb076e488b9cf5b6d3dbc36cebf3f
Si Hammond <sdhammo@sandia.gov>
2016-08-11 12:31:34 -0600

    Implement memkind_malloc for Vectors

--- openmp-opt-memkind/src/Vector.hpp
+++ openmp-opt-memkind/src/Vector.hpp
@@ -33,32 +33,15 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <sys/mman.h>
-#define HUGE_PAGE_SIZE (2 * 1024 * 1024)
-#define ALIGN_TO_PAGE_SIZE(x) \
-    (((x) + HUGE_PAGE_SIZE - 1) / HUGE_PAGE_SIZE * HUGE_PAGE_SIZE)
+
+#include <memkind.h>
 
 namespace miniFE {
 
 #ifdef MINIFE_HUGE_PAGES
 void *malloc_huge_pages(size_t size)
 {
-    // Use 1 extra page to store allocation metadata
-    // (libhugetlbfs is more efficient in this regard)
-    size_t real_size = ALIGN_TO_PAGE_SIZE(size + HUGE_PAGE_SIZE);
-    char *ptr = (char *)mmap(NULL, real_size, PROT_READ | PROT_WRITE,
-                  MAP_PRIVATE | MAP_ANONYMOUS |
-                  MAP_POPULATE | MAP_HUGETLB, -1, 0);
-    if (ptr == MAP_FAILED) {
-	   fprintf(stderr, "MFE: Allocation of huge pages for vector failed, resorting to malloc...\n");
-           // The mmap() call failed. Try to malloc instead
-           ptr = (char *)malloc(real_size);
-           if (ptr == NULL) return NULL;
-           real_size = 0;
-    }
-    // Save real_size since mmunmap() requires a size parameter
-    *((size_t *)ptr) = real_size;
-    // Skip the page with metadata
-    return ptr + HUGE_PAGE_SIZE;
+    return (double*) memkind_malloc(MEMKIND_HBW, size);
 }
 #endif
 
